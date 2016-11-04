edb06a12f77a88324d3d59a6ad7508dfb373632a
Oddle <joonhui@gmail.com>
2016-11-04 15:57:39 +0800

    remove theme's breadcrumb
    remove theme's breadcrumb

--- header.php
+++ header.php
@@ -27,12 +27,7 @@
   </head>
 
   <body>
-    <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
-        <?php if(function_exists('bcn_display'))
-        {
-            bcn_display();
-        }?>
-    </div>
+    
 
     <div class="blog-masthead">
       <div class="container">
