706cae8161cfba4922b6bc367ca49e2d0cc9e794
Oddle <joonhui@gmail.com>
2016-11-07 16:43:39 +0800

    added deferred code
    added deferred code

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'wordpresscustom_scripts' );
 
 // Add scripts and stylesheets
 function wordpresscustom_scripts() {
@@ -17,4 +18,15 @@ function wordpresscustom_scripts() {
 	// wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
 }
 
-add_action( 'wp_enqueue_scripts', 'wordpresscustom_scripts' );
+
+// Defer Javascripts
+// Defer jQuery Parsing using the HTML5 defer property
+if (!(is_admin() )) {
+    function defer_parsing_of_js ( $url ) {
+        if ( FALSE === strpos( $url, '.js' ) ) return $url;
+        if ( strpos( $url, 'jquery.js' ) ) return $url;
+        // return "$url' defer ";
+        return "$url' defer onload='";
+    }
+    add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+}
