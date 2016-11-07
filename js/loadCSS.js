55b7b282e0dd1895ecc266f0100c1b71e9b404ee
Oddle <joonhui@gmail.com>
2016-11-07 18:00:37 +0800

    fixed live css
    fixed live css

--- js/loadCSS.js
+++ js/loadCSS.js
@@ -37,6 +37,6 @@ if(window.location.host.includes('localhost')) {
   loadCSS( "//" + window.location.host + "/wordpress/wp-content/themes/wordpresscustom/css/bootstrap.min.css" );
   loadCSS( "//" + window.location.host + "/wordpress/wp-content/themes/wordpresscustom/css/blog.css" );
 } else {
-  loadCSS( "//" + window.location.host + "/wp-content/themes/wordpresscustom/css/bootstrap.min.css" );
-  loadCSS( "//" + window.location.host + "/wp-content/themes/wordpresscustom/css/blog.css" );
+  loadCSS( "//" + window.location.host + "/wp-content/themes/wordpress-staging/css/bootstrap.min.css" );
+  loadCSS( "//" + window.location.host + "/wp-content/themes/wordpress-staging/css/blog.css" );
 }
